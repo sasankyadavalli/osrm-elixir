db07843527ed2828050fa99d1a8fdc72363fcebf
Aditya Sanka <aditya@commut.co>
2017-04-12 14:55:02 +0530

    Initial commit with basic scaffolding.

---
+++ lib/osrm.ex
@@ -0,0 +1,18 @@
+defmodule OSRM do
+  @moduledoc """
+  Documentation for OSRM.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> OSRM.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/osrm_test.exs
@@ -0,0 +1,8 @@
+defmodule OSRMTest do
+  use ExUnit.Case
+  doctest OSRM
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
