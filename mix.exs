07863375565978175d8cb5bbd3c3b6a01b683fe0
Aditya Sanka <aditya@commut.co>
2017-04-12 15:05:56 +0530

    Add package and contributors info to mix.exs
    Add :credo and :ex_doc as local deps.

--- mix.exs
+++ mix.exs
@@ -3,12 +3,15 @@ defmodule OSRM.Mixfile do
 
   def project do
     [app: :osrm,
-     version: "0.1.0",
+     version: "0.0.1",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: description(),
+     package: package(),
      deps: deps()]
   end
+
 
   # Configuration for the OTP application
   #
@@ -16,6 +19,18 @@ defmodule OSRM.Mixfile do
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
     [extra_applications: [:logger]]
+  end
+
+  defp description do
+    """
+    Elixir bindings for OSRM.
+    """
+  end
+
+  defp package do
+    [maintainers: ["Commut", "Aditya Sanka"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/smart-commut/osrm-elixir"}]
   end
 
   # Dependencies can be Hex packages:
@@ -28,6 +43,8 @@ defmodule OSRM.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [# Local dependencies
+     {:credo, "~> 0.6", only: [:dev, :test ]},
+     {:ex_doc, "~> 0.15", only: [:dev, :test]},]
   end
 end
